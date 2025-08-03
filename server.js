884df486bfbc0522689fa354ec75fe41b1ea9d1f
RTZxWarHeadOP <Warhead12345677@gmail.com>
2025-08-03 20:50:34 +0530

    yeah this includes logrequest

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express  = require('express');
 const app = express();
 const db = require('./db');
-
+const passsport= require('passport');
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());// it will save data in req.body
 
 const Person = require('./models/Person');
 
+// logRequest.js
+const logRequest = (req, res, next) => {
+  const now = new Date().toISOString();
+  console.log(`[${now}] ${req.method} ${req.url}`);
+  next(); // Move to the next middleware or route handler
+};
+
+app.use(logRequest); 
 
 app.get('/',(req,res)=>{
   res.send('heya wassup boiiiiiiii')
@@ -20,4 +28,4 @@ app.use('/person',personRoutes);
 const PORT = 3001;
 app.listen(PORT,()=>{
    console.log('yup the sever is working')
-})
+}) 
